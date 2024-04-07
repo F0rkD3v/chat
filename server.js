e443f6300af8c270ebf773eeb3198483c279f9f0
F0rkD3v <danur2006@gmail.com>
2024-04-07 17:23:25 +0300

    My first chat vershon yo brooow

---
+++ server.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+const pathToIndex = path.join(__dirname, 'static', 'index.html');
+const indexHtmlFile = fs.readFileSync(pathToIndex);
+
+const scriptJsFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleCssFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
+
+const server = http.createServer((req, res) => {
+switch(req.url){
+    case '/': return res.end(indexHtmlFile); break;
+    case '/script.js': return res.end(scriptJsFile); break;
+    case '/style.css': return res.end(styleCssFile); break;
+    }
+    res.statusCode = 404;
+    return res.end('Error 404');
+});
+
+server.listen(3000);
---
+++ static/script.js
@@ -0,0 +1,1 @@
+alert('Brawl Stars');
